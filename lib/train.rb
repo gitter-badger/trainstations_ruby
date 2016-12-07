23875a75afcb59f5ac7368e22925fac0c139b26c
Diego Suarez and Joshua Rinard <joshua@>
2016-12-07 11:17:06 -0800

    finish unit spec and methods for train based on CRUD

--- lib/train.rb
+++ lib/train.rb
@@ -1,10 +1,48 @@
 class Train
-  attr_reader(:route)
+  attr_reader(:route, :id)
 
   define_method(:initialize) do |attributes|
     @route = attributes.fetch(:route)
-
+    @id = attributes.fetch(:id, nil)
   end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO train (route) VALUES ('#{@route}') RETURNING id;")
+    @id = result.first.fetch("id").to_i()
+  end
+
+  define_singleton_method(:all) do
+    results = DB.exec("SELECT * FROM train;")
+    trains = []
+    results.each() do |result|
+      id = result.fetch("id").to_i
+      route = result.fetch("route")
+      trains.push(Train.new({:route => route, :id => id}))
+    end
+    trains
+  end
+
+  define_method(:==) do |other_train|
+    self.route().==(other_train.route()).&(self.id().==(other_train.id()))
+  end
+
+  define_method(:delete) do
+    DB.exec("DELETE FROM train WHERE id = #{self.id()};")
+  end
+
+  define_method(:update) do |attributes|
+    @route = attributes.fetch(:route)
+    @id = self.id()
+    p @route, self.id
+    DB.exec("UPDATE train SET route = '#{@route}' WHERE id = #{@id};")
+  end
+
+
+
+
+
+
+
 
 
 end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -8,4 +8,51 @@ describe(Train) do
       expect(test_train.route()).to(eq("green"))
     end
   end
+  describe('#save') do
+    it "save it to database" do
+      test_train = Train.new({:route => "green"})
+      test_train.save()
+      expect(Train.all()).to(eq([test_train]))
+    end
+  end
+  describe('.all') do
+    it "select all trains from database" do
+      test_train1 = Train.new({:route => "green"})
+      test_train1.save()
+      test_train2 = Train.new({:route => "blue"})
+      test_train2.save()
+      expect(Train.all()).to(eq([test_train1, test_train2]))
+    end
+  end
+
+  describe('#==') do
+    it "overides equals" do
+      test_train1 = Train.new({:route => "green"})
+      test_train2 = Train.new({:route => "green"})
+      expect(test_train1).to(eq(test_train2))
+    end
+  end
+
+  describe("#delete") do
+    it "deletes train from database" do
+      test_train2 = Train.new({:route => "blue"})
+      test_train2.save()
+      test_train2.delete()
+      expect(Train.all()).to(eq([]))
+    end
+  end
+
+  describe("#update") do
+    it "lets you update train in the database" do
+      test_train = Train.new({:route => "blue"})
+      test_train.save()
+      test_train.update({:route => "green"})
+      expect(test_train.route).to(eq("green"))
+    end
+  end
+
+
+
+
+
 end
